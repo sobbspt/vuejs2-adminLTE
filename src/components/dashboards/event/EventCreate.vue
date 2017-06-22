90950ebf6cf172938707b4254cb7b72ec13a1564
Wanichnun Sinpitak <kitaboons@gmail.com>
2017-06-22 18:30:57 +0700

    UPDATE

--- src/components/dashboards/event/EventCreate.vue
+++ src/components/dashboards/event/EventCreate.vue
@@ -1,20 +1,107 @@
 <template>
   <div>
-    <h1>Event Create</h1>
+    <section class="content-header">
+      <h1>Event Create</h1>
+    </section>
+    <section class="content">
+      <div class="col-sm-6">
+        <div class="box box-info">
+          <div class="box-header with-border">
+            <h3 class="box-title">Create event</h3>
+          </div>
+          <!-- /.box-header -->
+          <!-- form start -->
+          <form class="form-horizontal">
+            <div class="box-body">
+
+              <div class="form-group">
+                <label for="inputName" class="col-sm-2 control-label">Name</label>
+                <div class="col-sm-10">
+                  <input v-model="name" type="text" class="form-control" id="inputName" placeholder="Name">
+                </div>
+              </div>
+
+              <div class="form-group">
+                <label for="inputDescription" class="col-sm-2 control-label">Description</label>
+
+                <div class="col-sm-10">
+                  <input v-model="description" type="text" class="form-control" id="inputDescription" placeholder="Description">
+                </div>
+              </div>
+            </div>
+
+            <div class="overlay-dukdik" v-if="isLoading">
+              <div class="loading-dukdik">
+                <pacman-loader></pacman-loader>
+              </div>
+            </div>
+
+            <!-- /.box-body -->
+            <div class="box-footer">
+              <button type="button" @click="clearInput()" class="btn btn-default">Cancel</button>
+              <button type="button" @click="addEvent()" class="btn btn-info pull-right">Create</button>
+            </div>
+            <!-- /.box-footer -->
+          </form>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
+  import PacmanLoader from 'vue-spinner/src/PacmanLoader.vue'
   export default {
     name: 'eventCreate',
+    components: {
+      PacmanLoader
+    },
     data () {
       return {
+        name: '',
+        description: '',
+        saveSuccess: '',
+        isLoading: false
+      }
+    },
+    firebase: {
 
+    },
+    methods: {
+      addEvent: function () {
+        var vm = this;
+        vm.isLoading = true;
+        db.ref('/events/' + this.name).set({
+          name: this.name,
+          description: this.description
+        }).then(function () {
+          vm.isLoading = false;
+        })
+      },
+      clearInput: function () {
+        var vm = this;
+        vm.name = "";
+        vm.description = "";
       }
     }
   }
 </script>
 
 <style>
+  .overlay-dukdik {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    z-index: 9999;
+    opacity: 0.7;
+    top: 0;
+    left: 0;
+    background-color: #000000;
+  }
 
+  .loading-dukdik {
+    position: absolute;
+    top: 50%;
+    left: 50%
+  }
 </style>
--- src/components/dashboards/event/EventList.vue
+++ src/components/dashboards/event/EventList.vue
@@ -1,6 +1,55 @@
 <template>
   <div>
-    <h1>Event List</h1>
+    <section class="content-header">
+      Event List
+    </section>
+    <section class="content">
+      <div class="box">
+        <div class="box-header">
+          <h3 class="box-title">Hover Data Table</h3>
+        </div>
+        <!-- /.box-header -->
+        <div class="box-body">
+          <div id="example2_wrapper" class="dataTables_wrapper form-inline dt-bootstrap"><div class="row"><div class="col-sm-6"></div><div class="col-sm-6"></div></div><div class="row"><div class="col-sm-12">
+            <table id="example2" class="table table-bordered table-hover dataTable" role="grid" aria-describedby="example2_info">
+              <thead>
+              <tr role="row">
+                <th class="sorting_asc" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Rendering engine: activate to sort column descending">Name</th>
+                <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Browser: activate to sort column ascending">Description</th>
+              </tr>
+              </thead>
+              <tbody>
+                <tr v-for="event in events" role="row" class="odd">
+                  <td class="sorting_1">{{ event.name }}</td>
+                  <td>{{ event.description }}</td>
+                </tr>
+              </tbody>
+              <tfoot>
+              </tfoot>
+            </table>
+          </div>
+          </div>
+            <div class="row">
+              <div class="col-sm-5">
+                <div class="dataTables_info" id="example2_info" role="status" aria-live="polite">Total events: {{ events.length }}
+                </div>
+              </div>
+              <div class="col-sm-7">
+                <div class="dataTables_paginate paging_simple_numbers" id="example2_paginate">
+                  <ul class="pagination">
+                    <li class="paginate_button active">
+                      <a href="#" aria-controls="example2" data-dt-idx="1" tabindex="0">
+                        1</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- /.box-body -->
+      </div>
+    </section>
   </div>
 </template>
 
@@ -11,6 +60,9 @@
       return {
 
       }
+    },
+    firebase: {
+      events: db.ref('events')
     }
   }
 </script>
